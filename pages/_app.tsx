c1e652430f7652ee8252a0329668544eebd07ec4
伊東和成 <itokazunari5375@icloud.com>
2022-07-12 15:31:28 +0900

    認証周り全般

---
+++ pages/_app.tsx
@@ -0,0 +1,21 @@
+import "../styles/globals.css";
+import { Auth0Provider } from "@auth0/auth0-react";
+
+function MyApp({ Component, pageProps }) {
+  //ログイン後のリダイレクト先を指定
+  const redirectUri = `${process.env["NEXT_PUBLIC_BASE_URL"]}/login`;
+  console.log(redirectUri);
+  console.log(process.env["NEXT_PUBLIC_AUTH0_DOMAIN"]);
+  return (
+    <Auth0Provider
+      domain={process.env["NEXT_PUBLIC_AUTH0_DOMAIN"]!}
+      clientId={process.env["NEXT_PUBLIC_AUTH0_CLIENT_ID"]!}
+      audience={process.env["NEXT_PUBLIC_AUTH0_AUDIENCE"]!}
+      redirectUri={redirectUri}
+    >
+      <Component {...pageProps} />
+    </Auth0Provider>
+  );
+}
+
+export default MyApp;
